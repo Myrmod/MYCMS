0a0394a92c66e9cbb8da9129d3810276abd99517
Pascal Galle <pascal.galle@gmx.de>
2021-05-04 16:41:53 +0200

    add basic menu

---
+++ src/lib/components/Header/Menu/index.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+</script>
+
+<nav>
+  <ul>
+    <li>
+      <a href="settings" preload="true"> Settings </a>
+    </li>
+    <li>
+      <a href="api-creator" preload="true"> API Creator </a>
+    </li>
+    <li>
+      <a href="api-preview" preload="true"> API Preview </a>
+    </li>
+  </ul>
+</nav>
+
+<style lang="stylus">
+  nav
+    height 100%
+
+    ul
+      display flex
+      padding 0
+      margin 0
+      gap 1rem
+      height 100%
+
+      li
+        list-style none
+        height 100%
+
+        a
+          height 100%
+          display flex
+          text-decoration none
+          justify-content center
+          align-items center
+          padding 0 1rem
+</style>
--- src/lib/components/Header/index.svelte
+++ src/lib/components/Header/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { authenticated } from '$lib/stores/userStore'
+  import Menu from './Menu/index.svelte'
 
   async function logout() {
     const res = await fetch('/auth/logout')
@@ -10,5 +11,17 @@
 </script>
 
 <header>
+  <Menu />
   <button type="button" on:click={logout}> Logout </button>
 </header>
+
+<style lang="stylus">
+  header
+    display flex
+    height 5rem
+    align-items center
+    padding 0 1rem
+
+    button
+      margin-left auto
+</style>
