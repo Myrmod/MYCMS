08573f68b4d48c24797c34af90e0d0a30b8cfdd9
Pascal Galle <pascal.galle@gmx.de>
2021-05-06 20:49:26 +0200

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-const sveltePreprocess = require('svelte-preprocess')
-const node = require('@sveltejs/adapter-node')
+import sveltePreprocess from 'svelte-preprocess'
+import node from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess({
